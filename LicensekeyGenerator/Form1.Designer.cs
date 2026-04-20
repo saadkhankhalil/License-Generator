470a3a2c7fb1172158856a590df6a2c64d1772f5
saadkhankhalil <muhammadsaad03060509835@gmail.com>
2026-04-20 12:12:13 +0500

    License Generator

---
+++ LicensekeyGenerator/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace LicensekeyGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtClient = new System.Windows.Forms.TextBox();
+            this.txtMachine = new System.Windows.Forms.TextBox();
+            this.dtExpiry = new System.Windows.Forms.DateTimePicker();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtClient
+            // 
+            this.txtClient.Location = new System.Drawing.Point(237, 113);
+            this.txtClient.Name = "txtClient";
+            this.txtClient.Size = new System.Drawing.Size(281, 20);
+            this.txtClient.TabIndex = 0;
+            // 
+            // txtMachine
+            // 
+            this.txtMachine.Location = new System.Drawing.Point(237, 167);
+            this.txtMachine.Name = "txtMachine";
+            this.txtMachine.Size = new System.Drawing.Size(281, 20);
+            this.txtMachine.TabIndex = 1;
+            // 
+            // dtExpiry
+            // 
+            this.dtExpiry.Location = new System.Drawing.Point(237, 221);
+            this.dtExpiry.Name = "dtExpiry";
+            this.dtExpiry.Size = new System.Drawing.Size(281, 20);
+            this.dtExpiry.TabIndex = 2;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(282, 284);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(164, 41);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(152, 73);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(15, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "**";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Client Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Machine Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(152, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Set Expiry Date";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(671, 44);
+            this.panel1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(152, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(367, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "License Generator App";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 393);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.dtExpiry);
+            this.Controls.Add(this.txtMachine);
+            this.Controls.Add(this.txtClient);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtClient;
+        private System.Windows.Forms.TextBox txtMachine;
+        private System.Windows.Forms.DateTimePicker dtExpiry;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LicensekeyGenerator/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LicensekeyGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string client = txtClient.Text.Trim();
+                string machine = txtMachine.Text.Trim();
+                string expiry = dtExpiry.Value.ToString("yyyy-MM-dd");
+
+                string data = $"CLIENT={client}|MACHINE={machine}|EXPIRES={expiry}";
+                string privateKey = File.ReadAllText("privatekey.xml");
+                string signature = SignData(data, privateKey);
+
+                string licenseContent = $"{data}|SIGN={signature}";
+                string fileName = $"{client}_license.dat";
+                File.WriteAllText(fileName, licenseContent);
+
+                lblStatus.Text = $"✅ License generated: {fileName}";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error: " + ex.Message;
+            }
+        }
+        private static string SignData(string data, string privateKey)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.FromXmlString(privateKey);
+                byte[] bytes = Encoding.UTF8.GetBytes(data);
+                byte[] sig = rsa.SignData(bytes, new SHA256CryptoServiceProvider());
+                return Convert.ToBase64String(sig);
+            }
+        }
+    }
+}
+
